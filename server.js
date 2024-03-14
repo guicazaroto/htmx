607fda30f36a76b01fa2f5b1da8940af1a4117d5
Guilherme Guimarães Cazaroto <gui.cazaroto@gmail.com>
2024-03-14 17:52:01 -0300

    add props

--- server.js
+++ server.js
@@ -6,28 +6,45 @@ app.use(express.static('public'));
 app.use(urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/users', (req, res) => {
-  const users = [
-    {
-      id: 1,
-      name: 'John Doe',
-      email: 'jon@gmail.com'
-    },
-    {
-      id: 2,
-      name: 'Jane Doe',
-      email: 'jan@gmail.com'
-    }
-  ];
+const users = [
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'jon@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Jane Doe',
+    email: 'jan@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Jim Doe',
+    email: 'j@e.com'
+  },
+  {
+    id: 4,
+    name: 'Jill Doe',
+    email: 'u@i.com'
+  }
+];
 
+app.get('/users', (_, res) => {
   const template = `
     <h1>Users</h1>
     <ul>
       ${users.map(user => `<li>${user.name}</li>`).join('')}
     </ul>
   `;
-  
+
   res.send(template);
+})
+
+app.get('/users/async', (req, res) => {
+  console.log(req.query.limit)
+  setTimeout(() => {
+    res.json(users.slice(0, req.query.limit));
+  }, 2000);
 })
 
 app.listen(3000, () => {
